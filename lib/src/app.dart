caa3870825a1ce1641e67e9ba8b27814fdf05663
willcavenagli_tsbr <willian.cavenagli@t-systems.com.br>
2019-07-30 22:12:14 -0300

    Mode Banner Removed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/login_screen/login_screen.dart';
 
 class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: LoginScreen(),
       ),
